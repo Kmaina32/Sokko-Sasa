3e9a83562e6e9f7b7d0cc3fa925bb98023c75210
Ubuntu Academy <kishansan28@gmail.com>
2025-09-16 13:34:46 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeiC1fLWb-zoh_lSS1j-qal8f3LX5VelM",
+  authDomain: "studio-4244987249-1f3ac.firebaseapp.com",
+  projectId: "studio-4244987249-1f3ac",
+  storageBucket: "studio-4244987249-1f3ac.appspot.com",
+  messagingSenderId: "909303382851",
+  appId: "1:909303382851:web:5d351f32b6f234a7e1e5f6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
