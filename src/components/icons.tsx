312d9037831c569e56fc1720c3cd402936d9b8cd
Ubuntu Academy <kishansan28@gmail.com>
2025-09-16 13:21:05 +0000

    use this as my logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,15 +1,31 @@
 export const SokkoSasaLogo = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg 
-        {...props}
-        viewBox="0 0 100 100" 
-        xmlns="http://www.w3.org/2000/svg"
-    >
-        <path fill="#FF9800" d="M20 70h60v15H20z" />
-        <path fill="#FF5722" d="M25 85h50v5H25z" />
-        <path fill="#FFFFFF" d="M30 75h40v5H30z" />
-        <path fill="#FFC107" d="M50 10 C 35 10, 30 30, 30 40 L 70 40 C 70 30, 65 10, 50 10z" />
-        <path fill="#FF9800" d="M30 40h40v30H30z" />
-        <path fill="#FFFFFF" d="M40 50h20v10H40z" />
-    </svg>
-  );
-  
+  <svg
+    {...props}
+    viewBox="0 0 160 50"
+    xmlns="http://www.w3.org/2000/svg"
+    fontFamily="sans-serif"
+    fontWeight="bold"
+  >
+    <path
+      fill="#F97316"
+      d="M45.66 43.43c-3.1-1.63-4.3-5.2-2.68-8.3l10-18c1.62-3.1 5.2-4.3 8.3-2.68l4.42 2.33c.8.42 1.15 1.37.72 2.17l-13.8 25.5c-.42.8-1.37 1.15-2.17.72l-4.79-2.74z"
+    />
+    <path
+      stroke="#000"
+      strokeWidth="5"
+      strokeLinecap="round"
+      d="M37.5 13.5A7.5 7.5 0 0 1 52.5 13.5"
+      fill="none"
+    />
+    <path
+      fill="#000"
+      d="M26.24 35.34a2.5 2.5 0 0 1-3.37-1.12L12.02 14.1a2.5 2.5 0 0 1 4.5-2.58l10.85 20.12a2.5 2.5 0 0 1-1.13 3.7z"
+    />
+    <text x="65" y="42" fontSize="24" fill="black">
+      Sokko
+    </text>
+    <text x="122" y="42" fontSize="24" fill="#F97316">
+      Sasa
+    </text>
+  </svg>
+);
